932e032caaa5e450a96205abd8a0ed8e1eb87c19
unknown <ruither.carvalho@gmail.com>
2021-04-03 18:10:53 -0300

    gerenciador de cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { StarComponent } from './star/star.component';
+import { CourseListComponent } from './courses/course-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
