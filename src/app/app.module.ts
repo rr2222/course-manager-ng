ab8f0d6a82c463e25fd6e0e3e2c49b6efb39f07d
unknown <ruither.carvalho@gmail.com>
2021-04-11 18:09:21 -0300

    criado nav-bar, rotas e pagina de erro 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { Error404Component } from './error-404/error-404.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,16 +7,34 @@ import { NgModule } from '@angular/core';
 import { FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo:"courses", pathMatch: "full"
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: Error404Component
+      }
+
+    ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
